c22e5adb82dc233b2a7da0eefdc62ea030cd24c0
Bogdaniy <kukharchuk.bogdan0@gmail.com>
2023-09-11 14:43:05 +0200

    add new file

---
+++ Task2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            string randomNumber = Convert.ToBase64String(RandomNumberGenerator.GenerateRandomNumber(32));
+            Console.WriteLine(randomNumber);
+        }
+        Console.ReadLine();
+    }
+}
+
+public class RandomNumberGenerator
+{
+    public static byte[] GenerateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+}
+
