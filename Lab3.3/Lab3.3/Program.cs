14b6eed0d8514168ae46e20b91f8188fcbcab7cc
Bohdaniy <kukharchuk.bogdan0@gmail.com>
2023-09-24 16:59:33 +0200

    LAB3

---
+++ Lab3.3/Lab3.3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string filetext = "D:\\labitos2\\Lab3\\main.dat";
+        string fileContent = File.ReadAllText(filetext);
+        Console.WriteLine("Змiст файлу:");
+        Console.WriteLine(fileContent);
+        if (File.Exists(filetext))
+        {
+            var bytes = Encoding.UTF8.GetBytes("Mit21");
+            string text = "D:\\labitos2\\Lab3\\main.dat";
+            var filetextt = File.ReadAllBytes(text);
+            var key = new byte[5];
+            for (int i = 0; i <= filetextt.Length - 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    key[j] = (byte)(filetextt[i + j] ^ bytes[j]);
+                }
+
+                var decryptedText = DecryptedText(key, filetextt, i);
+
+                if (Contains(decryptedText))
+                {
+                    Console.WriteLine($"{i} Дешифрований текст: {Encoding.UTF8.GetString(decryptedText)}");
+                }
+
+            }
+
+        }
+    }
+        public static byte[] DecryptedText(byte[] key, byte[] filetextt, int index)
+        {
+            var decryptedText = new byte[filetextt.Length];
+
+            for (int i = index; i < filetextt.Length; i += key.Length)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (i + j > decryptedText.Length - 1)
+                    {
+                        break;
+                    }
+                    decryptedText[i + j] = (byte)(filetextt[i + j] ^ key[j]);
+                }
+            }
+            index -= 1;
+            for (int a = index; a >= 0;)
+            {
+                for (int i = key.Length - 1; i >= 0; i--)
+                {
+                    if (a < 0)
+                    {
+                        break;
+                    }
+                    decryptedText[a] = (byte)(filetextt[a] ^ key[i]);
+                    a--;
+                }
+            }
+
+            return decryptedText;
+        }
+
+        public static bool Contains(byte[] mesage)
+        {
+            var mit = Encoding.UTF8.GetString(mesage);
+            return mit.Contains("Mit21");
+        }
+    }
+
+
+
---
+++ Lab3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+     
+    static void Main()
+    {
+        string filetext = "D:\\labitos2\\Lab3\\text.txt";
+            string fileContent = File.ReadAllText(filetext);
+            Console.WriteLine("Змiст файлу:");
+            Console.WriteLine(fileContent);
+            Console.WriteLine("Введiть ключ:");
+            string key = Console.ReadLine();
+
+        try
+        {
+
+           
+            
+            if (File.Exists(filetext))
+            {
+                byte[] textBytes = Encoding.UTF8.GetBytes(fileContent);
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+                byte[] encryptedText = new byte[textBytes.Length];
+
+                for (int i = 0; i < textBytes.Length; i++)
+                {
+                    encryptedText[i] = (byte)(textBytes[i] ^ keyBytes[i % keyBytes.Length]);
+                }
+
+                using (FileStream stream = new FileStream("D:\\labitos2\\Lab3\\main.dat", FileMode.OpenOrCreate))
+                {
+                    stream.Write(encryptedText, 0, encryptedText.Length);
+                }
+
+                Console.WriteLine("Текст був записаний у новий файл ");
+                string newFiletext = "D:\\labitos2\\Lab3\\main.dat";
+                string newfileContent = File.ReadAllText(newFiletext);
+                Console.WriteLine(newfileContent);
+                Console.WriteLine("Чи бажаєте ви розшифрувати текст(y/n): ");
+                 char g = Console.ReadLine()[0];
+                if(g=='y' || g=='Y'){
+                    
+                    string FilePath = "D:\\labitos2\\Lab3\\main.dat";
+                    if (File.Exists(FilePath))
+                    {
+                        
+                        byte[] KeyBytess = Encoding.UTF8.GetBytes(key);
+
+                        byte[] decryptedText;
+
+                        using (FileStream stream = new FileStream("D:\\labitos2\\Lab3\\main.dat", FileMode.Open))
+                        {
+                            decryptedText = new byte[stream.Length];
+                            stream.Read(decryptedText, 0, (int)stream.Length);
+                        }
+
+                        byte[] Text = new byte[decryptedText.Length];
+
+                        for (int i = 0; i < decryptedText.Length; i++)
+                        {
+                            Text[i] = (byte)(decryptedText[i] ^ KeyBytess[i % KeyBytess.Length]);
+                        }
+
+                        string text = Encoding.UTF8.GetString(Text);
+
+                        Console.WriteLine("Розшифрований текст: " + text);
+                    }
+                }
+                else if (g == 'n' || g=='N')
+                {
+                    Console.WriteLine("До побачення!");
+                    Environment.Exit(0);
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не знайдено.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+        
+
+        
+    }
+
+
+}
